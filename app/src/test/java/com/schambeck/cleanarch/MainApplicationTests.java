b519052e5e8ad524d906ca25c2e98c5baac7d76b
Marcelo Schambeck <mschambeck@gmail.com>
2022-02-21 00:12:37 -0300

    Integration test

---
+++ app/src/test/java/com/schambeck/cleanarch/MainApplicationTests.java
@@ -0,0 +1,14 @@
+package com.schambeck.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// No content
+	}
+
+}
